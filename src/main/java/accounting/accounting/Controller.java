72b87fc8b65465cc99900d97489ee0ff85061ef9
Tristan-BS <98057075+Tristan-BS@users.noreply.github.com>
2024-10-25 12:46:30 +0200

    Changelog 25.10.2024 - 12:45
    -> Designed UI in Scenebuilder
    -> Made first Pressed Function Connections
    -> Min and Max Size of Window

--- src/main/java/accounting/accounting/Controller.java
+++ src/main/java/accounting/accounting/Controller.java
@@ -1,8 +1,75 @@
 package accounting.accounting;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.image.*;
+import javafx.scene.layout.*;
 
 public class Controller {
 
+    // ImageViews
+    @FXML
+    private ImageView IV_Menu;
+    @FXML
+    private ImageView IV_Home;
+    @FXML
+    private ImageView IV_Invoices;
+    @FXML
+    private ImageView IV_Documents;
+    @FXML
+    private ImageView IV_Evaluation;
+    @FXML
+    private ImageView IV_Account;
+    @FXML
+    private ImageView IV_Exit;
+
+    // Button for the ImageViews
+    @FXML
+    private Button B_Menu;
+    @FXML
+    private Button B_Home;
+    @FXML
+    private Button B_Invoices;
+    @FXML
+    private Button B_Documents;
+    @FXML
+    private Button B_Evaluation;
+    @FXML
+    private Button B_Account;
+    @FXML
+    private Button B_Exit;
+
+    // AnchorPane
+    @FXML
+    private AnchorPane AP_SBText;
+
+    // Methods for Button Pressed
+    @FXML
+    private void On_B_Menu_Pressed() {
+        System.out.println("Home Button Pressed");
+    }
+    @FXML
+    private void On_B_Home_Pressed() {
+        System.out.println("Home Button Pressed");
+    }
+    @FXML
+    private void On_B_Invoices_Pressed() {
+        System.out.println("Invoices Button Pressed");
+    }
+    @FXML
+    private void On_B_Documents_Pressed() {
+        System.out.println("Documents Button Pressed");
+    }
+    @FXML
+    private void On_B_Evaluation_Pressed() {
+        System.out.println("Evaluation Button Pressed");
+    }
+    @FXML
+    private void On_B_Account_Pressed() {
+        System.out.println("Account Button Pressed");
+    }
+    @FXML
+    private void On_B_Exit_Pressed() {
+        System.exit(0);
+    }
 }
--- src/main/java/accounting/accounting/Main.java
+++ src/main/java/accounting/accounting/Main.java
@@ -11,8 +11,10 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("CKAccounting.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+        stage.setMinHeight(620);
+        stage.setMinWidth(920);
+        stage.setTitle("CK Accounting System");
         stage.setScene(scene);
         stage.show();
     }
