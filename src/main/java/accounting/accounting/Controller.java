db030e61e177e2c95b1c9861ed8de8e6d052bc34
Tristan-BS <98057075+Tristan-BS@users.noreply.github.com>
2024-10-28 21:53:53 +0100

    Changelog 28.10.2024 - 21:53
    -> Added "Main" Page
    -> Fixed Animation of the Menu
    -> Added Animation with responsive size of the MainPage

--- src/main/java/accounting/accounting/Controller.java
+++ src/main/java/accounting/accounting/Controller.java
@@ -1,11 +1,14 @@
 package accounting.accounting;
 
-import javafx.animation.TranslateTransition;
+import javafx.animation.*;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.image.*;
 import javafx.scene.layout.*;
 import javafx.util.Duration;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Controller {
 
@@ -44,28 +47,50 @@ public class Controller {
     // AnchorPane
     @FXML
     private AnchorPane AP_SBText;
+    @FXML
+    private AnchorPane AP_Page;
 
     private boolean isMenuOpen = false;
+    private int MenuWidth = 75;
 
     // Methods for Button Pressed
+
+    // Menu Button Pressed with animation
     @FXML
     private void On_B_Menu_Pressed() {
-        System.out.println("Home Button Pressed");
-
+        // Menu Animation
         TranslateTransition transition = new TranslateTransition();
-        transition.setDuration(Duration.millis(250)); // Duration of the animation
-        transition.setNode(AP_SBText); // Node to be animated
+        transition.setDuration(Duration.millis(200));
+        transition.setNode(AP_SBText);
+
+        // Page Animation
+        Timeline timeline = new Timeline();
+        KeyFrame keyFrame;
 
         if (isMenuOpen) {
-            transition.setByX(125); // Change in X position (positive value to move right)
-            transition.play();
+            keyFrame = new KeyFrame(
+                    Duration.millis(200),
+                    new KeyValue(AP_Page.prefWidthProperty(), AP_Page.getPrefWidth() - MenuWidth), // Set width to original width
+                    new KeyValue(AP_Page.layoutXProperty(), AP_Page.getLayoutX() + MenuWidth) // Set x position the original x position
+            );
+            transition.setByX(125);
             isMenuOpen = false;
         } else {
-            transition.setByX(-125); // Change in X position (negative value to move left)
-            transition.play();
+            keyFrame = new KeyFrame(
+                    Duration.millis(200),
+                    new KeyValue(AP_Page.prefWidthProperty(), AP_Page.getPrefWidth() + MenuWidth), // Set width to original width + MenuWidth
+                    new KeyValue(AP_Page.layoutXProperty(), AP_Page.getLayoutX() - MenuWidth) // Set x position to original x position - MenuWidth
+            );
+            transition.setByX(-125);
             isMenuOpen = true;
         }
+
+        timeline.getKeyFrames().add(keyFrame);
+        timeline.play();
+        transition.play();
     }
+
+
     @FXML
     private void On_B_Home_Pressed() {
         System.out.println("Home Button Pressed");
